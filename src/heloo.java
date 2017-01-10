51dfce285bffa1f31fb7c3b3e639d6e091fb8900
alex <mamayko.1998@mail.ru>
2017-01-11 01:11:33 +0300

    2 commit

--- src/heloo.java
+++ src/heloo.java
@@ -4,6 +4,7 @@
 public class heloo {
 
     public static void main(String[] args) {
-        System.out.println(new holo().pipi());
+        System.out.println(new holo().pipi()+"sdfsfdsdf");
+        //проба гитаdffdsfssgfdsf
     }
 }
