306c2715a5ae77e1a74a65138398bad834d3462b
alex mamayko <alex mamayko>
2017-01-11 01:00:29 +0300

    First commit

---
+++ src/heloo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by alex on 10.01.2017.
+ */
+public class heloo {
+
+    public static void main(String[] args) {
+        System.out.println(new holo().pipi());
+    }
+}
---
+++ src/holo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by alex on 10.01.2017.
+ */
+public class holo {
+    public static String pipi ()
+    {
+        String s= "123123";
+        return s;
+    }
+}
